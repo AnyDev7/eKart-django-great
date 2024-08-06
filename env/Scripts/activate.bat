a9610a71eaca55daa6b84ff1f67cca3c347c81f9
AnyDev7 <anydev7@outlook.com>
2024-08-06 11:53:38 -0600

    Nuevo env

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\AnyDev7\OneDrive\PythonDev\eKart\greatkart\kart\env
+set VIRTUAL_ENV=C:\Users\anyde\OneDrive\PythonDev\eKart\greatkart\kart\env
 
 if not defined PROMPT set PROMPT=$P$G
 
